168fad958019a468935e0068bd317b501647a79c
SpaghettiOverload <imyours@mail.bg>
2022-06-15 11:27:29 +0300

    Adding UTILS and API structure

---
+++ src/utils/api/getBalance.ts
@@ -0,0 +1,21 @@
+/* eslint-disable import/prefer-default-export */
+import axios from 'axios';
+import { GET_BALANCE_URL } from './endpoints';
+
+const getWalletBalance = async (walletAccount: string) => {
+    const url: string = `${GET_BALANCE_URL(walletAccount)}`
+    return axios.get(url)
+                .then(response => {
+                    let balances: any[] = []
+                    if (response.status === 200 && response.data.result) {
+                        response.data.result.map((type: { denom: any; amount: any; }) => {
+                            balances.push({
+                                denom: type.denom,
+                                amount: type.amount
+                            })
+                        })
+                    }
+                    return { balances }
+                })
+}
+export default getWalletBalance
